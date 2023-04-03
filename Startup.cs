80e5da127fa585c843f384350883bfa36ee8fbbb
wma4432 <mapken@watchtower-security.com>
2023-04-03 06:09:56 -0500

    clean up

--- Startup.cs
+++ Startup.cs
@@ -32,22 +32,7 @@ namespace promptNess
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        Configuration.GetConnectionString("DefaultConnection")));
-
-services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-services.AddAuthentication()
-    .AddGoogle(options =>
-    {
-        IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
-
-        options.ClientId = googleAuthNSection["ClientId"];
-        options.ClientSecret = googleAuthNSection["ClientSecret"];
-    });
+            services.AddRazorPages();     
 
         }
 
